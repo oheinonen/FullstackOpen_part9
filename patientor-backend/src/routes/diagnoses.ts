b4a5cd43e95350bf8f39410407b7e50dd23c7651
Oskari Heinonen <oskari.l.heinonen@helsinki.fi>
2023-09-02 12:08:22 +0300

    task 9.10

---
+++ patientor-backend/src/routes/diagnoses.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import diagnoseService from '../services/diagnoses';
+const router = express.Router();
+
+router.get('/', (_req, res) => {
+  const diagnoses = diagnoseService.getDiagnoses();
+  res.send({ diagnoses });
+});
+
+export default router;
---
+++ patientor-backend/src/types.ts
@@ -0,0 +1,5 @@
+export interface Diagnose {
+  name: string;
+  code: string;
+  latin?: string;
+}
