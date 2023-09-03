2d909cb549a6000ca291d5d5dbf7c67710c36bef
Oskari Heinonen <oskari.l.heinonen@helsinki.fi>
2023-09-03 17:58:03 +0300

    task 9.24

--- patientor-backend/src/routes/diagnoses.ts
+++ patientor-backend/src/routes/diagnoses.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import diagnosisService from '../services/diagnoses';
 const router = express.Router();
 
-router.get('/', (_req, res) => {
-  res.send(diagnosisService.getDiagnosis());
+router.get('/', (req, res) => {
+  const codes = req.query.codes as string[];
+  const diagnoses = diagnosisService.getDiagnosis(codes);
+  res.send(diagnoses);
 });
 
 export default router;
--- patientor-backend/src/services/diagnoses.ts
+++ patientor-backend/src/services/diagnoses.ts
@@ -1,8 +1,10 @@
 import diagnosisData from '../data/diagnoses';
 import { Diagnosis } from '../types';
 
-const getDiagnosis = () : Diagnosis[] => {
-  return diagnosisData;
+const getDiagnosis = (codes?: string[]) : Diagnosis[] => {
+  return codes
+    ? diagnosisData.filter((diagnosis) => codes.includes(diagnosis.code))
+    : diagnosisData;
 };
 
 export default {
